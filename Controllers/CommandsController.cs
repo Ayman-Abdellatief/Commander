a21257df51128fd685a986b226a4286ea11a266b
Ayman <aymn.abdellatief@gmail.com>
2020-12-25 16:39:42 +0200

    DATA TRANSFER OBJECTS & CREATE ENDPOINT

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,26 +12,32 @@ namespace Commander.Controllers
     public class CommandsControllers : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsControllers(ICommanderRepo repository)
+        public CommandsControllers(ICommanderRepo repository , IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         //Get api/commands
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
         {
-            var commandItems = _repository.GetAppCommands();
-            return Ok(commandItems);
+            var commandItems = _repository.GetAllCommands();
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
          
          //Get api/commands/{id}
          [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int Id) 
+        public ActionResult <CommandReadDto> GetCommandById(int Id) 
         {
             var commandItem = _repository.GetCommandById(Id);
-            return Ok(commandItem);
+            if(commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<CommandReadDto>(commandItem));
 
         }
 
